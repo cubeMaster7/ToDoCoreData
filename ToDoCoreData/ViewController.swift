73a37aa2608e82ae939420af340430b4cb34605f
Paul James <ienglishman187@gmail.com>
2021-10-27 19:15:04 +0300

    Initial Commit

---
+++ ToDoCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoCoreData
+//
+//  Created by Paul James on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
